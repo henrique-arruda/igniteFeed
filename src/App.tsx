6a1de3e77cdec22b63d57405b2b4279bcd432730
Henrique Arruda <rick.arruda@hotmail.com>
2023-02-02 17:19:33 -0300

    Aplicação completa :)

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-10-03 20:00:00')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/henrique-arruda.png',
+      name: 'Henrique Arruda',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-10-10 20:00:00')
+  },
+];
+
+export function App() {
+  return (
+   <div>
+    <Header />
+
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+       {posts.map(post => {
+        return (
+          <Post 
+          key={post.id}
+          author={post.author}
+          content={post.content}
+          publishedAt={post.publishedAt}
+          />
+        )
+       })}
+      </main>
+       </div>
+  </div>
+  )
+}
+
+
+
